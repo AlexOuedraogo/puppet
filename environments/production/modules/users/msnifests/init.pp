010ed19a4bca86d45217a8fbf19240ecf04f70d9
Alex Ouedraogo <alex@ouedraogo.nl>
2015-04-20 20:48:28 -0400

    aa

--- environments/production/modules/users/msnifests/init.pp
+++ environments/production/modules/users/msnifests/init.pp
@@ -1,5 +1,5 @@
 class users {
-user { 'gui':
+ user { 'gui':
   ensure           => 'present',
   comment          => 'gui',
   password         => '$6$T5YjaeSdk9KmW1ww$zxzjkJmcsR8Ch3MuHlj3LbC5RCmvwWo0r3/Z1ek7wspNt6LdBf6PYW.BQg19bX0X04wo3zXy9HGBE9NOcS4i1.',

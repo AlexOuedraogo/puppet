7639fe0feb60dd44bd6bf340852b301df7d35b6c
root <root@zandoma.lan>
2015-04-21 03:13:57 +0200

    aanpassingen toegevoegd

--- environments/production/modules/puppet/manifests/papply.sh
+++ environments/production/modules/puppet/manifests/papply.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo puppet apply /root/git/puppet/environments/production/modules/puppet/manifests/site.pp --modulepath=/root/git/puppet/environments/production/modules/ $*
+sudo puppet apply /root/git/puppet/environments/production/modules/puppet/manifests/site.pp --modulepath=/root/git/puppet/environments/production/modules 

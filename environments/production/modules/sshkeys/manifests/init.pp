77c84677bb2c549c7f0c79ab5b8a8de67a28520f
root <root@lintiba-001.yatenga.nl>
2015-04-22 18:26:38 -0400

    added sshkeys module

--- environments/production/modules/sshkeys/manifests/init.pp
+++ environments/production/modules/sshkeys/manifests/init.pp
@@ -3,10 +3,12 @@ class sshkeys {
 
   file { '/home/ouedraogoa/.ssh':
     ensure => directory,
+    mode   => 0755
 }
   file { '/home/ouedraogoa/.ssh/authorized_keys':
     ensure => file,
     source => 'puppet:///modules/sshkeys/rsa_id.pub',
+    mode   => 0766
 }
 }
 

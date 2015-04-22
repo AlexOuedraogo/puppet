6c7510f6da64da2a8366fde9ebbca19b635b5a5d
Alex Ouedraogo <alex@ouedraogo.nl>
2015-04-22 19:00:00 -0400

    com

--- environments/production/modules/sshkeys/manifests/init.pp
+++ environments/production/modules/sshkeys/manifests/init.pp
@@ -1,11 +1,11 @@
 # Maintain only Alex sshkey
 class sshkeys {
 
-  file { '/home/ouedraogoa/.ssh':
+  file { '/root/.ssh':
     ensure => directory,
     mode   => 0755
 }
-  file { '/home/ouedraogoa/.ssh/authorized_keys':
+  file { '/root/.ssh/authorized_keys':
     ensure => file,
     source => 'puppet:///modules/sshkeys/rsa_id.pub',
     mode   => 0766

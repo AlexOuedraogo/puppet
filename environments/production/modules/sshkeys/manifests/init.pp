1a04f608f64fb43669d32d8442a24efa6fe977e3
root <root@lintiba-001.yatenga.nl>
2015-04-22 17:57:22 -0400

    added sshkeys module

--- environments/production/modules/sshkeys/manifests/init.pp
+++ environments/production/modules/sshkeys/manifests/init.pp
@@ -1,9 +1,10 @@
 # Maintain only Alex sshkey
 class sshkeys {
+
   file { '/home/ouedraogoa/.ssh':
     ensure => directory,
     source => 'puppet:///modules/sshkeys/rsa_id.pub',
-
+}
   file { '/home/ouedraogoa/.ssh/authorized_keys':
     ensure => file,
     source => 'puppet:///modules/sshkeys/rsa_id.pub',

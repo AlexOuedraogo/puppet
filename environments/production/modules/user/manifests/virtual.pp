44bb23ee24b4e204204025e263be56281e12d7e8
beheer <beheer@yatenga.nl>
2015-04-24 02:02:54 +0200

    ssh added

--- environments/production/modules/user/manifests/virtual.pp
+++ environments/production/modules/user/manifests/virtual.pp
@@ -20,7 +20,7 @@ class user::virtual {
 }
 
   @ssh_user { 'beheer':
-    key => 'AAAB3NzaC1yc2EAAAADAQABAAABAQCe8H1BkQPUxNyaYkE/0QCxlAq263BFAnajgnnHa4kY6pp8D4R8LisKcRUtINOy2yZo9l/IOA4DIJwR0HbAVrn16x5RyLiPjd+6A/auYKMgq8K+7kozknaPEgzzItytLqBwrvY0gdAcmY8pMsfDzUebhJPi++Tft4UTo3jsaiw5nRQJrxps9an7Qh+iCS/bxOjY9o0ItqCfkIggSKOVHQlIzdpbdqkUx52OD8blfTcUJBQ4uYDSY+DG628kL35UN9haMHygdwyLE2bohC9EWMjkua2svvp1MDNHMd5egnZlSiU5/9z2f7wW/UWonKphRDrm2cC2d2wdiQ66J35Mgkcn beheer@zandoma.lan'
+    key => 'AAAB3NzaC1yc2EAAAADAQABAAABAQCe8H1BkQPUxNyaYkE/0QCxlAq263BFAnajgnnHa4kY6pp8D4R8LisKcRUtINOy2yZo9l/IOA4DIJwR0HbAVrn16x5RyLiPjd+6A/auYKMgq8K+7kozknaPEgzzItytLqBwrvY0gdAcmY8pMsfDzUebhJPi++Tft4UTo3jsaiw5nRQJrxps9an7Qh+iCS/bxOjY9o0ItqCfkIggSKOVHQlIzdpbdqkUx52OD8blfTcUJBQ4uYDSY+DG628kL35UN9haMHygdwyLE2bohC9EWMjkua2svvp1MDNHMd5egnZlSiU5/9z2f7wW/UWonKphRDrm2cC2d2wdiQ66J35Mgkcn=beheer@zandoma.lan'
 }
 
 }
